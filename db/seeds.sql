bcb098c68f25e766028286638f342ee60532e35f
abasheryassin <abasheryassin@gmail.com>
2023-01-22 15:31:43 -0600

    seed data

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (name)
+VALUES
+(Legal),
+(HR),
+(Enginneering),
+(Sales);
+
+INSERT INTO role (title, salery, department_id)
+VALUES
+("Lawer", 150000, 1),
+("Lagal Lead", 200000, 1),
+("HR Representative", 100000, 2),
+("Lead Software Engineer", 200000, 3),
+("Junior Software Engineer", 150000, 3),
+("Sales Lead", 120000, 4),
+("Sales Associate", 80000, 4);
+
+INSERT INTO emplyoee (first_name, last_name)
+VALUES
+("Sam", "Baker"),
+("Justin", "Jerfferson"),
+("Walt", "Disney"),
+("Yassin", "Abasher"),
+("Rachel", "Curren"),
+("Kayla", "Taylor"),
+("Moti", "Begna");
